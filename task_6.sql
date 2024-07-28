bdf8582e90e600b352869d0304bdd33461ba90ab
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-07-28 21:16:20 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -10,10 +10,10 @@ VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
 
 -- Insert the second customer record
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 -- Insert the third customer record
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
 -- You can add more INSERT statements for additional customer records.
